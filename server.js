c722a883e14f014972ffec9c58f68a5b43d82eb5
salini-360 <salinisoundaiyan@gmail.com>
2026-05-20 15:33:03 +0530

    update server.js

--- server.js
+++ server.js
@@ -179,8 +179,10 @@ io.on("connection", (socket) => {
 
 /* ---------------- SERVER ---------------- */
 
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on port 5000");
+server.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
 
 });
